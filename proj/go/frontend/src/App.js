2d965e4f777f82191f684b62e38fa7aec1048d23
lion <eoals46@daum.net>
2020-09-24 19:42:13 +0900

    Go Based Web

---
+++ proj/go/frontend/src/App.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import CardContainer from './ProductCards';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from './Navigation';
+import { SignInModalWindow, BuyModalWindow } from './modalwindows';
+import About from './About';
+import Orders from './orders';
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        loggedin: false,
+        name: "",
+      }
+    };
+    this.showSignInModalWindow = this.showSignInModalWindow.bind(this);
+    this.toggleSignInModalWindow = this.toggleSignInModalWindow.bind(this);
+    this.showBuyModalWindow = this.showBuyModalWindow.bind(this);
+    this.toggleBuyModalWindow = this.toggleBuyModalWindow.bind(this);
+  }
+
+  handleSignedIn(user) {
+    this.setState({
+      user: user
+    });
+  }
+
+  showSignInModalWindow(){
+    const state = this.state;
+    const newState = Object.assign({},state,{showSignInModal:true});
+    this.setState(newState);
+  }
+
+  toggleSignInModalWindow() {
+    const state = this.state;
+    const newState = Object.assign({},state,{showSignInModal:!state.showSignInModal});
+    this.setState(newState);
+  }
+
+
+  showBuyModalWindow(id,price){
+    const state = this.state;
+    const newState = Object.assign({},state,{showBuyModal:true,productid:id,price:price});
+    this.setState(newState);
+  }
+
+  toggleBuyModalWindow(){
+    const state = this.state;
+    const newState = Object.assign({},state,{showBuyModal:!state.showBuyModal});
+    this.setState(newState);
+  }
+
+  componentDidMount() {
+    fetch('user.json')
+        .then(res => res.json())
+        .then((result) => {
+          console.log('Fetch...');
+          this.setState({
+            user: result
+          });
+        });
+  }
+
+  render() {
+    return (
+        <div>
+          <Router>
+            <div>
+              <Nav user={this.state.user} showModalWindow={this.showSignInModalWindow}/>
+              <div className='container pt-4 mt-4'>
+                <Route exact path="/"
+                       render={() => <CardContainer location='cards.json'
+                                                    showBuyModal={this.showBuyModalWindow} />} />
+                <Route path="/promos"
+                       render={() => <CardContainer location='promos.json' promo={true}
+                                                    showBuyModal={this.showBuyModalWindow}/>} />
+                {this.state.user.loggedin ? <Route path="/myorders"
+                                                   render={()=><Orders location='user.json'/>}/> : null}
+                <Route path="/about" component={About} />
+              </div>
+              <SignInModalWindow showModal={this.state.showSignInModal}
+                                 toggle={this.toggleSignInModalWindow}/>
+              <BuyModalWindow showModal={this.state.showBuyModal}
+                              toggle={this.toggleBuyModalWindow}
+                              user={this.state.user.ID}
+                              productid={this.state.productid} price={this.state.price}/>
+            </div>
+          </Router>
+        </div>
+    );
+  }
+}
+
+export default App;
